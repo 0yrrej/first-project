6b23793bf0c49348d8690c31ac05d9a2dd2d348d
Jerry Fonseca <jerryf55988@students.laalliance.org>
2017-11-20 06:31:05 +0000

    done

---
+++ sketch.js
@@ -0,0 +1,13 @@
+function setup() {
+    createCanvas(800, 800)
+}
+
+function draw() {
+    noFill()
+    strokeWeight(5.5)
+    ellipse(50, 50, 80, 80);
+    ellipse(95, 85, 80, 80);
+    ellipse(140, 50, 80, 80);
+    ellipse(185, 85, 80, 80);
+    ellipse(230, 50, 80, 80);
+}
